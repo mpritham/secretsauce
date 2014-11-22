a7f2bab774f085ed5396a9de53bf7a8d7f449d81
Pritham Marupaka <pritham@v1020-wn-240-14.campus-dynamic.uwaterloo.ca>
2014-11-21 21:00:16 -0500

    Added downloads

--- secret.py
+++ secret.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from werkzeug import secure_filename
 from models import Entry
 import pdb
@@ -7,12 +7,15 @@ import pdb
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 UPLOAD_FOLDER = 'uploads'
 
+#Entries dict
+entries = []
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello():
-    entries = [Entry("meow","test","test"),Entry("test","test","test")]
+    entries.append(Entry("meow","test","test"))
     return render_template('index.html', entries=entries)
 
 @app.route('/save', methods=['GET', 'POST'])
@@ -21,8 +24,10 @@ def upload_file():
         file = request.files['file']
         if file:
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "TEST" 
+            filePath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            entries.append(Entry(filename, filePath, "Some Description"))
+            file.save(filePath)
+            return "Thanks for uploading"
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -32,6 +37,12 @@ def upload_file():
          <input type=submit value=Upload>
     </form>
     '''
+@app.route('/getFile', methods=['GET'])
+def get():
+    return Response(open('uploads/test.c'),
+                       mimetype="multipart/mixed",
+                       headers={"Content-Disposition":
+                                    "attachment;filename="+"test.c"})
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run()
