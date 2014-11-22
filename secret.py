7e47b28a5b0185951bcac540ae8d29738e552df2
Andrew Codispoti <andrewcod749@gmail.com>
2014-11-21 19:22:27 -0500

    added class for datamodel

--- secret.py
+++ secret.py
@@ -7,3 +7,16 @@ def hello():
 
 if __name__ =="__main__":
     app.run()
+class Entry:
+    def __init__(self,title, path,description):
+        self.title=title
+        self.path=path
+        self.description=description
+    def getTitle():
+        return self.title
+    def getDescription():
+        return self.description
+    def getPath():
+        return self.path
+        
+        
